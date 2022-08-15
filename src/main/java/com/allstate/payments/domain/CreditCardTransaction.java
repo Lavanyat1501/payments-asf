c053acd7505a6f930e6910eb9e07ddc0d8281dd1
matt <matt@multicode.co.uk>
2022-08-15 13:17:52 +0000

    starting testing

--- src/main/java/com/allstate/payments/domain/CreditCardTransaction.java
+++ src/main/java/com/allstate/payments/domain/CreditCardTransaction.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name="cctransactions")
@@ -129,5 +130,18 @@ public class CreditCardTransaction {
                 ", taxRate=" + taxRate +
                 ", type='" + type + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreditCardTransaction that = (CreditCardTransaction) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
